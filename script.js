139d349347b7bfd5edece32e0936cbb7bd832347
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-04 09:36:34 -0800

    add content to html which are divs with id and started connecting js  to html with variables equaling the id from html

---
+++ script.js
@@ -0,0 +1,4 @@
+var start = document.querySelector("#start")
+var quiz = document.querySelector("#quiz")
+var question = document.querySelector("#question")
+var choices = document.querySelector("#choices")
