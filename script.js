e6f297f3ecc9b6174ab2d8405389e9997c33629b
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-05 10:33:33 -0800

    add function to display question and answer choices

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 // created variables that connect back to my HTML
 // will use the varobles to create the game on javscript
 
-var start = document.querySelector("#start");
+var instructions = document.querySelector("#instructions");
+var startBtn = document.querySelector("#start-btn");
 var quiz = document.querySelector("#quiz");
 var question = document.querySelector("#question");
+
 var choiceA = document.querySelector("#A");
 var choiceB = document.querySelector("#B");
 var choiceC = document.querySelector("#C");
 var choiceD = document.querySelector("#D");
-var counter = document.querySelector("#counter");
-var progress = document.querySelector("#progress");
-var scoreCard = document.querySelector("#scoreContainer");
+
+var timeCounter = document.querySelector(".time-counter");
+var notice = document.querySelector("#notice");
+
+var restartBtn = document.querySelector(".restart-btn");
+var correctCounter = 0;
+var incorrectCounter = 0;
+var timer = 60;
+var questionIndex = 0;
+
 
 // using the variable questions to connect var question  with choice (A,B,C,D)
 var questions = [
@@ -46,12 +55,39 @@ var questions = [
         question : "What is && in JavaScript?",
         choiceA : "loops",
         choiceB : "datatype",
-        choiceC : "srting",
+        choiceC : "string",
         choiceD : "boolean",
         correct : "C",
     }
-] ;
+];
+console.log(questions)
 
-var lastQuestion = questions.length - 1;
-var renderquestion = 0;
+// write a function for the start button to begin game
+function startGame() {
+    instructions.style.display = "none";
+    displayQuestion()
+}
 
+startBtn.addEventListener("click", startGame)
+
+function displayQuestion() {
+    quiz.style.display = "block"
+    question.textContent = questions[questionIndex].question
+    choiceA.textContent = questions[questionIndex].choiceA
+    choiceB.textContent = questions[questionIndex].choiceB
+    choiceC.textContent = questions[questionIndex].choiceC
+    choiceD.textContent = questions[questionIndex].choiceD
+}
+
+function checkAnswer() {
+
+}
+
+// checkAnswer, in this function, comparing correct answer and user seleted answer
+// increase index
+// call displayQuesiotn() for dispalying next question.
+
+// 74 75 declare new global variable
+// create a on click check answer function
+// index variable
+// 0 is hard code instead or that you can use your variable
